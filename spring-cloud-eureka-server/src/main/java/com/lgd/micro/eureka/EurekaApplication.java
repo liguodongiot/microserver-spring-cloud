bd3a93c4d95c473758d0bbaf053e82a3b48042b2
guodong.li <guodong.li@msxf.com>
2018-01-23 17:15:48 +0800

    add

---
+++ spring-cloud-eureka-server/src/main/java/com/lgd/micro/eureka/EurekaApplication.java
@@ -0,0 +1,23 @@
+package com.lgd.micro.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <p>Project: microserver-spring-cloud</p>
+ * <p>Package: PACKAGE_NAME</p>
+ * <p>Title: </p>
+ * <p>Description: </p>
+ *
+ * @author guodong.li
+ * @version 1.0.0
+ * @date 2018/1/23 16:40 星期二
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
