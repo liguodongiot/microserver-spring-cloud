1078dfbf82ba219c4182729b30d839f522936ae8
guodong.li <guodong.li@msxf.com>
2018-01-23 21:51:42 +0800

    init

---
+++ movie-prodiver/src/main/java/com/lgd/microservice/prodiver/ProdiverApplication.java
@@ -0,0 +1,12 @@
+package com.lgd.microservice.prodiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdiverApplication.class, args);
+	}
+}
---
+++ movie-prodiver/src/test/java/com/lgd/microservice/prodiver/ProdiverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lgd.microservice.prodiver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProdiverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
