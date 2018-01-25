40f986e2f82ae8becae414dddf841de2d3a504b4
guodong.li <guodong.li@msxf.com>
2018-01-25 15:11:34 +0800

    mark

---
+++ spring-cloud-config-client/src/main/java/com/lgd/micro/config/ConfigClientApplication.java
@@ -0,0 +1,33 @@
+package com.lgd.micro.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>Project: microserver-spring-cloud</p>
+ * <p>Package: com.lgd.micro.config</p>
+ * <p>Title: </p>
+ * <p>Description: </p>
+ *
+ * @author guodong.li
+ * @version 1.0.0
+ * @date 2018/1/25 15:07 星期四
+ */
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
